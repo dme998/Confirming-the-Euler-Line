dde9634db4487a2dadec4315d819ff486c2e689f
Daniel Eggers <38550843+dme998@users.noreply.github.com>
2021-02-02 09:41:16 -0600

    user inputs triangle's three points
    Triangle build() function takes input from user using Scanner and performs validation.
    Added getInputDouble() method to help streamline this. 
    Added email address.

--- Main.java
+++ Main.java
@@ -1,18 +1,19 @@
 /*
 Title: Confirming the Euler Line
 Description: This program takes user 2D points and reports the Euler line from special points on a triangle.
-
 Authors: Daniel Eggers, Peter Galloway
-Email: [daniel eggers' email], ptgxfv@mail.umsl.edu
+Email: dme998@mail.umsl.edu, ptgxfv@mail.umsl.edu
 Course: CS 4500, Section 001
 Date: 02/08/2021
-
 File: Main.java
 Language: Java 8 (IDE: Java)
 File Dependencies: none
 Created extra files: none
 Resources: Java Docs - https://docs.oracle.com/javase/8/docs/api/
+https://coderanch.com/t/668346/java/Catch-InputMismatchException-working
  */
+
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -49,11 +50,36 @@ class Point {
 class Triangle {
     Point A, B, C;
     Point centroid, circumcenter, orthocenter;
-
+    
+    private static double getInputDouble(String message) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(message);
+        while(!input.hasNextDouble()) {
+            System.out.print("Invalid, try again: ");
+            input.next();
+        }
+        return input.nextDouble();
+    }
+    
     void build() {
         /* prompt for A, B, C,
             error message then re-prompt on invalid input
          */
+        
+        double x1 = getInputDouble("Enter first point X: ");
+        double y1 = getInputDouble("Enter first point Y: ");
+        Point A = new Point(x1, y1);
+        System.out.println("Point A is " + A.toString());
+    
+        double x2 = getInputDouble("Enter second point X: ");
+        double y2 = getInputDouble("Enter second point Y: ");
+        Point B = new Point(x1, y1);
+        System.out.println("Point B is " + B.toString());
+        
+        double x3 = getInputDouble("Enter third point X: ");
+        double y3 = getInputDouble("Enter third point Y: ");
+        Point C = new Point(x3, y3);
+        System.out.println("Point C is " + C.toString());
 
         /* if collinear
             error message,

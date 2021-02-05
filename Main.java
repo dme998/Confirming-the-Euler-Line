e5364616fe35d978223e2cb1b0ac5af9f3472e1f
Peter Galloway <49027051+PeterGalloway@users.noreply.github.com>
2021-02-04 21:08:42 -0600

    update Main.java Added collinear method.

--- Main.java
+++ Main.java
@@ -18,16 +18,57 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         /* print description */
+        testTriangles();
 
-        Triangle myTriangle = new Triangle();
+//        Triangle myTriangle = new Triangle();
+
         /* build triangle function */
-        myTriangle.build();
+//        myTriangle.build();
 
         /* print special points function */
-        myTriangle.printSpecialPoints();
+//        myTriangle.printSpecialPoints();
 
         /* distance error function */
-        myTriangle.distanceError();
+//        myTriangle.distanceError();
+    }
+
+    static void testTriangles() {
+        Triangle right = new Triangle(
+                new Point(0,0),
+                new Point(0, 2),
+                new Point(2,2),
+                "right triangle"
+        );
+        Triangle isosceles = new Triangle(
+                new Point(0,0),
+                new Point(0, 2),
+                new Point(1,1),
+                "isosceles triangle"
+        );
+        Triangle equilateral = new Triangle(
+                new Point(0,0),
+                new Point(1, 0),
+                new Point(0.5, (Math.sqrt(3))/2),
+                "equilateral triangle"
+        );
+        Triangle verticalLine = new Triangle(
+                new Point(0,0),
+                new Point(0, 2),
+                new Point(0,12),
+                "vertical line"
+        );
+        Triangle horizontalLine = new Triangle(
+                new Point(3,1),
+                new Point(4, 1),
+                new Point(6,1),
+                "horizontal line"
+        );
+        Triangle slopedLine = new Triangle(
+                new Point(0,0),
+                new Point(1,1),
+                new Point(2,2),
+                "sloped line"
+        );
     }
 }
 
@@ -50,7 +91,18 @@ class Point {
 class Triangle {
     Point A, B, C;
     Point centroid, circumcenter, orthocenter;
-    
+
+    Triangle() {
+        // empty
+    }
+
+    Triangle(Point a, Point b, Point c, String type) {
+        A = a;
+        B = b;
+        C = c;
+
+        System.out.println("Collinear: " + collinear() + ", " + type);
+    }
     private double getInputDouble(String message) {
         Scanner input = new Scanner(System.in);
         System.out.print(message);
@@ -65,17 +117,17 @@ class Triangle {
         /* prompt for A, B, C,
             error message then re-prompt on invalid input
          */
-        
+
         double x1 = getInputDouble("Enter first point X: ");
         double y1 = getInputDouble("Enter first point Y: ");
         A = new Point(x1, y1);
         System.out.println("Point A is " + A);
-    
+
         double x2 = getInputDouble("Enter second point X: ");
         double y2 = getInputDouble("Enter second point Y: ");
         B = new Point(x2, y2);
         System.out.println("Point B is " + B);
-        
+
         double x3 = getInputDouble("Enter third point X: ");
         double y3 = getInputDouble("Enter third point Y: ");
         C = new Point(x3, y3);
@@ -85,44 +137,42 @@ class Triangle {
             error message,
             pause for enter then halt
          */
-        System.out.println("Collinear: " + collinear());
 //        slowExit();
 
         /* if equilateral
             print absolute value of largest distance between special points,
             pause for enter then halt
          */
-         
+//        slowExit();
+
         orthocenter = calculateOrthocenter();
         centroid = calculateCentroid();
         circumcenter = calculateCircumcenter();
-         
-//        slowExit();
+
     }
 
     void printSpecialPoints() {
-//        System.out.println("Orthocenter: " + orthocenter);
-//        System.out.println("Centroid: " + centroid);
-//        System.out.println("Circumcenter: " + circumcenter);
-
+        System.out.println("Orthocenter: " + orthocenter);
+        System.out.println("Centroid: " + centroid);
+        System.out.println("Circumcenter: " + circumcenter);
     };
     
     private double getSlope(double x1, double x2, double y1, double y2) {
         /* calculates slope or sets to undefined (NaN) if denominator is 0 */
         double m = (x2 - x1 != 0) ? (y2 - y1) / (x2 - x1) : Double.NaN;
-//        System.out.println("Slope is " + m);
+        System.out.println("Slope is " + m);
         return m;
     }
     private double getPerpSlope(double m) {
         /* calculates perpendicular slope */
         double p = (m != 0) ? (-1 * 1 / m) : 0;
-//        System.out.println("P-Slope is " + p);
+        System.out.println("P-Slope is " + p);
         return p;
     }
     private double getYIntercept(double m, double x, double y) {
         /* calculates for b in the equation, y = mx + b */
         double b = y - (m * x);
-//        System.out.println("Y-int of " + x + "," + y + " with slope " + m + " is " + b);
+        System.out.println("Y-int of " + x + "," + y + " with slope " + m + " is " + b);
         return b;
     }
     
@@ -155,7 +205,7 @@ class Triangle {
          */
         double ox = (pAB - pBC != 0) ? (bBC - bAB) / (pAB - pBC) : Double.NaN;
         double oy = (!Double.isNaN(ox)) ? (pAB * ox) + bAB : Double.NaN;
-        
+
         orthocenter = new Point(ox, oy);
         return orthocenter;
     }
@@ -182,7 +232,20 @@ class Triangle {
 
     /* check collinear function */
     boolean collinear() {
-        return false;
+        boolean isCollinear = false;
+
+        if ((A.getX() == B.getX()) && (C.getX() == A.getX())) {
+            // vertical line
+            isCollinear = true;
+        } else if ((A.getY() == B.getY()) && (C.getY() == A.getY())) {
+            // horizontal line
+            isCollinear = true;
+        } else {
+            double m = (B.getY() - A.getY())/(B.getX() - A.getX());
+            // y2 - y1 = m(x2 - x1)
+            isCollinear = (C.getY() - A.getY()) == m * (C.getX() - A.getX());
+        }
+        return isCollinear;
     }
 
     /* check equilateral function */
